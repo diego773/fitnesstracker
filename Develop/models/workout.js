c6f7701c31985b9974eb6b749b65502bb6491019
Diego <diegovillarreal123@gmail.com>
2021-06-08 16:25:51 -0500

    added models

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -2,9 +2,51 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserWorkoutSchema = new Schema({
-    username: {
-      type: String,
-      trim: true,
-      required: "Username is Required"
+// gets workouts in array
+const workoutSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  workout: [
+    {
+      name: {
+        type: String,
+        trim: true,
+      },
+      type: {
+        type: String,
+        trim: true,
+      },
+      weight: {
+        type: Number,
+        default: 0,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+      },
+      reps: {
+        type: Number,
+        default: 0,
+      },
+      duration: {
+        type: Number,
+        default: 0,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+      },
     },
+  ],
+
+  totalDuration: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = workout;
