030791137157f4a52404067fef0d9a2650bd6999
Diego <diegovillarreal123@gmail.com>
2021-06-08 16:34:06 -0500

    added api

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,12 +1,38 @@
 const router = require("express").Router();
 const Workout = require("../models/transaction.js");
 
+// create workout
 router.post("/api/workout", ({ body }, res) => {
   Workout.create(body)
-    .then((dbTransaction) => {
-      res.json(dbTransaction);
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
     .catch((err) => {
       res.status(400).json(err);
     });
 });
+
+// post workouts
+router.post("/api/workout/bulk", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+// finds the workouts
+router.get("/api/workout", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
